89093dd1b630c5aabe4b0b1be9843e65911b51e1
Ryan Luong <lumintriet@gmail.com>
2025-05-27 15:06:29 +0700

    Add prettier.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - [x] Create project with Hono and Bun
 - [x] Push to GitHub
 - [x] Add tseslint
+- [x] Add prettier
--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -11,13 +12,14 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
   },
   {
     // Disable type-checked rules for config files
     files: ["*.config.js", "*.config.ts"],
-    ...tseslint.configs.disableTypeChecked,
-  }
+    ...tseslint.configs.disableTypeChecked
+  },
+  eslintConfigPrettier
 );
